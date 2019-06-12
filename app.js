79288c76a997619fd04d7259af66aff9ba7eb728
els <navesllanes@gmail.com>
2019-06-12 14:53:39 +0100

    pellets and wood chips added

--- app.js
+++ app.js
@@ -61,8 +61,5 @@ app.get('/index.html/countries', function (req, res) {
  }
 
 
- var server = app.listen(8081, function(){
-     var host= server.address().address;
-     var port = server.address().port;
- });
+ var server = app.listen(8081);
 
